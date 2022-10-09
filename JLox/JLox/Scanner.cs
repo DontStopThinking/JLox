0dfea0891333d92b2d6f21e0d06bdc7a1ecf90f7
Shantanu Balse <infoshantanu02@gmail.com>
2022-10-09 13:19:55 +0100

    Handle some logical operators
    !=, ==, <=, >=

--- JLox/JLox/Scanner.cs
+++ JLox/JLox/Scanner.cs
@@ -48,6 +48,18 @@ namespace JLox
                 case '+': AddToken(TokenType.PLUS); break;
                 case ';': AddToken(TokenType.SEMICOLON); break;
                 case '*': AddToken(TokenType.STAR); break;
+                case '!':
+                    AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                    break;
+                case '=':
+                    AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                    break;
+                case '<':
+                    AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                    break;
+                case '>':
+                    AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                    break;
                 default:
                     Lox.Error(_line, $"Unexpected character '{c}'");
                     break;
@@ -63,8 +75,24 @@ namespace JLox
 
         private void AddToken(TokenType type, object? literal)
         {
-            string text = _source.Substring(_start, _current - _start);
+            string text = _source[_start.._current];
             _tokens.Add(new Token(type, text, literal, _line));
+        }
+
+        private bool Match(char expected)
+        {
+            if (IsAtEnd())
+            {
+                return false;
+            }
+
+            if (_source[_current] != expected)
+            {
+                return false;
+            }
+
+            _current++;
+            return true;
         }
     }
 }
